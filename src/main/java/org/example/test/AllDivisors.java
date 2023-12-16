be9c9591aaf383dac3495cd149955216e5aa8d78
stupidstick <stupidstick@yandex.ru>
2023-12-16 18:36:49 +0700

    Initial commit

---
+++ src/main/java/org/example/test/AllDivisors.java
@@ -0,0 +1,15 @@
+package org.example.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactorization {
+    public static int[] primeOf(int n) {
+        List<Integer> primeFactors = new ArrayList<>();
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/threadpool/PoolRequest.java
@@ -0,0 +1,7 @@
+package org.example.threadpool;
+
+public abstract class Req {
+    public void execute() {
+        System.out.println("aboba");
+    }
+}
